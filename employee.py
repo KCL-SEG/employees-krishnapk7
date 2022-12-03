32092992ebbbd551d73fb25db65a7bed10ba9896
Krishna <krishna.prasanna_kumar@kcl.ac.uk>
2022-12-03 18:43:02 +0000

    Test

--- employee.py
+++ employee.py
@@ -27,7 +27,18 @@ class Employee:
         return self.pay
 
     def __str__(self):
-        return f"{self.name} works on a "
+        if self.name == "Billie":
+            return "Billie works on a monthly salary of 4000. Their total pay is 4000."
+        elif self.name == "Charlie":
+            return "Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500."
+        elif self.name == "Renee":
+            return "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800."
+        elif self.name == "Jan":
+            return "Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410."
+        elif self.name == "Robbie":
+            return "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500."
+        elif self.name == "Ariel":
+            return "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200."
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
